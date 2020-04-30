ebc06246f83ee016a890e3235ea75a4c40ffb043
Miranda Lu <mirandahlu88@gmail.com>
2020-04-30 02:51:55 -0500

    checkpoint 18 sliders completed

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -15,6 +15,10 @@
     player.playPause(nextSong);
   });
 
+  $('#time-control input').on('input', function (event) {
+    player.skipTo(event.target.value);
+  });
+
   $('button#previous').on('click', function() {
     if (player.playState !== 'playing') {return;}
 
@@ -26,4 +30,12 @@
     player.playPause(previousSong);
   });
 
+  setInterval( () =>{
+    if (player.playState !== 'playing') {return;}
+    const currentTime = player.getTime();
+    const duration = player.getDuration();
+    const percent = (currentTime / duration) * 100;
+    $('#time-control .current-time').text(currentTime);
+    $('#time-control input').val(percent);
+  }, 1000);
 }
